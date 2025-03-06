e2d6dcea55b4b2a1a10c5e8f7d5802ddc60faeee
pavel <pavel.reznicek2@gmail.com>
2025-03-06 19:22:27 +0100

    changes

--- main.py
+++ main.py
@@ -1,3 +1,115 @@
-import requests
+import requests, csv, threading, time, sys
+from discord_webhook import DiscordWebhook, DiscordEmbed
+class Auto():
+    def __init__(self, email, sektor, qty):
+        self.email = email
+        self.sektor = sektor
+        self.qty = qty
+
+    def cart(self):
+        self.performanceId = "-2147476343"
+
+        self.r = requests.Session()
+
+        basket2 = True
+
+        while basket2:
+
+            try:
+                carts = self.r.get(f"https://www.ticketportal.cz/Event/SetNSeats/{self.performanceId}/{self.qty}?idsektor={self.sektor}&detail=true").json()
+            except:
+                Auto.cart(self)
+
+            self.basket = carts["ReturnedObject"]["NumberOfBasket"]
+
+            if self.basket == 0:
+                print("Not instock. Retrying")
+                time.sleep(0.5)
+                pass
+            else:
+                self.eventName = carts["ReturnedObject"]["Basket"]["Performances_info"][self.performanceId]["Event_name"]
+                self.sektorName = carts["ReturnedObject"]["Basket"]["Performance_count"][self.performanceId][0]["Sektor"]
+
+                jineID = carts["ReturnedObject"]["Basket"]["Performances_info"][self.performanceId]["ID_Podujatie"]
+                self.jineID = str(jineID)
+                break
+
+        Auto.checkout(self)
+
+    def checkout(self):
+
+        headers = {
+            "Host": "www.ticketportal.cz",
+            "Origin": "https://www.ticketportal.cz",
+            "referer": "https://www.ticketportal.cz/Basket",
+            "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1"
+        }
+        data = {
+            "AkciaADD": "",
+            f"typ_p_{self.jineID}": "1",
+            "MZZlavaNew": "",
+            "MZZlavaDel": "",
+            "MPksluzbeNew": "",
+            "zmazkosik": "0",
+            "DelMiesto": "",
+            "DelPredstavenie": "",
+            "GAeventNameBasket": {self.eventName},
+            "optionsRadiosPoistenie": "option2",
+            "pickupTypeOption": "7",
+            "email_pickup_7": self.email,
+            "other_email": "",
+            "email_pickup_1": "",
+            "heslo_pickup": "",
+            "CardpayPayOption": "17",
+            "s_garancia": "0.00",
+            "termsAccept_TicketportalTermsAccept": True,
+            "termsAccept_O2TermsAcceptRequired": True,
+            f"termsAccept_{self.performanceId}": True,
+            "createPayment": ""
+        }
+        try:
+            check = self.r.post("https://www.ticketportal.cz/basket", data=data)
+        except:
+            Auto.cart(self)
+        if "comgate" in check.url:
+            print(f"Successful Checkout, počet: {str(self.basket)}")
+            self.url = check.url
+            Auto.saveCSV(self)
+            Auto.webhook(self)
+        else:
+            print("Checkout failed. Retrying")
+            Auto.cart(self)
+
+    def saveCSV(self):
+        with open("success.csv", "a", newline="") as cfile:
+            fieldnames = ["LINK", "SEKTOR", "EMAIL", "QTY"]
+            write = csv.DictWriter(cfile, delimiter=',', fieldnames=fieldnames)
+            write.writerow({'LINK': self.url, 'SEKTOR': self.sektorName, "EMAIL": self.email, "QTY": self.basket})
+
+    def webhook(self):
+        try:
+            webhook = DiscordWebhook(
+                url="https://discord.com/api/webhooks/610221944574574594/ZNToXzLCBS3WhmNyllGQsi_90m2-_HPRPn2UTzo63c-aemo9wf7BYfRc7jo8nLc6JFEk")
+            embed = DiscordEmbed(title='CHECKOUT', color=5885036, url=self.url)
+            embed.add_embed_field(name='EVENT NAME', value=self.eventName, inline=True)
+            embed.add_embed_field(name='MISTA', value=self.sektorName, inline=True)
+            embed.add_embed_field(name='POCET', value=self.qty, inline=True)
+            embed.set_footer(text='TICKETPORTAL')
+            embed.set_timestamp()
+            webhook.add_embed(embed)
+            response = webhook.execute()
+        except:
+            print("Error with webhook")
 
 
+if __name__ == "__main__":
+    try:
+        with open('tasks.csv', "r", encoding='UTF-8', newline='') as csvfile:
+            tasks = csv.DictReader(csvfile, delimiter=',')
+            for line in tasks:
+                pico = Auto(email=line['EMAIL'], sektor=line["SEKTOR"], qty=line["QTY"])
+                t = threading.Thread(target=pico.cart).start()
+    except:
+        print('''Check your tasks.csv, exiting...''')
+        time.sleep(5)
+        sys.exit()
